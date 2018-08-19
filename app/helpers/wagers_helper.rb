2c58584d65dcc3014bb4abdcdf6091819cc062da
mklarman <mklarman@hotmail.com>
2018-08-18 23:58:19 -0400

    wager generation

--- app/helpers/wagers_helper.rb
+++ app/helpers/wagers_helper.rb
@@ -22,33 +22,33 @@ module WagersHelper
 
 		 			if t.sport == "NFL" 
 
-			 			@NFL_sides.push(t.sides_line) 
-						@NFL_totals.push(t.total_line) 
+			 			@nfl_sides.push(t.name) 
+						# @NFL_totals.push(t.total_line) 
 
 		 			elsif t.sport == "MLB" 
 
-			 			@MLB_sides.push(t.sides_line) 
-			 			@MLB_totals.push(t.total_line) 
+			 			@mlb_sides.push(t.name) 
+			 			# @MLB_totals.push(t.total_line) 
 
 		 			elsif t.sport == "NBA" 
 
-					 	@NBA_sides.push(t.sides_line) 
-			 			@NBA_totals.push(t.total_line) 
+					 	@nba_sides.push(t.name) 
+			 			# @NBA_totals.push(t.total_line) 
 
 		 			elsif t.sport == "NHL" 
 
-						 @NHL_sides.push(t.sides_line) 
-			 			 @NHL_totals.push(t.total_line) 
+						 @nhl_sides.push(t.name) 
+			 			  
 
 		 			elsif t.sport == "CBB" 
 
-			 			 @CBB_sides.push(t.sides_line) 
-			 			 @CBB_totals.push(t.total_line) 
+			 			 @cbb_sides.push(t.name) 
+			 			 # @CBB_totals.push(t.total_line) 
 
 		 			elsif t.sport == "CFB" 
 
-						 @CFB_sides.push(t.sides_line) 
-						 @CFB_totals.push(t.total_line) 
+						 @cfb_sides.push(t.name) 
+						 # @CFB_totals.push(t.total_line) 
 
 					 end 
 
@@ -81,43 +81,359 @@ module WagersHelper
 
 	  	def pick_type
 
-	  		if @client.bet_types == "Exotics" 
+	  		if @client.bet_types == "All"
+
+	  			@choices = [] 
 
 
-	  			if @client.sports[0] << @client.sports[1] << @client.sports[2] == "NFL" 
+	  			if @client.sports[0] << @client.sports[1] << @client.sports[2] == "NFL"
 
-	  				choices = []
-	  				nfl_sides = ["bears", "falcons", "giants"]
-	  				nfl_totals = [45, 60, 55, 34]
+	  				@nfl_sides.each do |n|
 
-	  				nfl_sides.each do NFL
+	  					@choices.push(n)
 
-	  					choices.push(NFL)
+	  				end		
 
+	  			end
+
+	  			if @client.sports[0] << @client.sports[1] << @client.sports[2] == "MLB"
+
+	  				@mlb_sides.each do |m|
+
+	  					@choices.push(m)
+	  					
 	  				end
+
+	  			end
+
+	  			if @client.sports[0] << @client.sports[1] << @client.sports[2] == "NBA"
+
+	  				@nba_sides.each do |n|
+
+	  					@choices.push(n)
+	  					
+	  				end
+
+	  			end
+
+	  			if @client.sports[0] << @client.sports[1] << @client.sports[2] == "NHL"
+
+	  				@nhl_sides.each do |n|
+
+	  					@choices.push(n)
+	  					
+	  				end
+
+	  			end
+
+	  			if @client.sports[0] << @client.sports[1] << @client.sports[2] == "CBB"
+
+	  				@cbb_sides.each do |c|
+
+	  					@choices.push(c)
+	  					
+	  				end
+
+	  			end
+
+	  			if @client.sports[0] << @client.sports[1] << @client.sports[2] == "CFB"
+
+	  				@cfb_sides.each do |c|
+
+	  					@choices.push(c)
+	  					
+	  				end
+
 	  			end
 
 	  			if @client.sports.length > 3
 
-		  			if @client.sports[8] + @client.sports[9] + @client.sports[10] == "NFL" 
+		  			if @client.sports[8] << @client.sports[9] << @client.sports[10] == "NFL" 
 
-		  				choices = []
-		  				nfl_sides = ["bears", "falcons", "giants"]
-		  				nfl_totals = [45, 60, 55, 34]
+		  				@nfl_sides.each do |n|
 
-		  				nfl_sides.each do NFL
+	  					@choices.push(n)
+	  					
+	  					end
+	  				end
 
-		  					choices.push(NFL)
-		  				end
 
-		  			end
+	  				if @client.sports[8] << @client.sports[9] << @client.sports[10] == "MLB" 
+
+		  				@mlb_sides.each do |m|
+
+	  					@choices.push(m)
+	  					
+	  					end
+	  				end
+
+	  				if @client.sports[8] << @client.sports[9] << @client.sports[10] == "NHL" 
+
+		  				@nhl_sides.each do |n|
+
+	  					@choices.push(n)
+	  					
+	  					end
+	  				end
+
+	  				if @client.sports[8] << @client.sports[9] << @client.sports[10] == "NBA" 
+
+		  				@nba_sides.each do |n|
+
+	  					@choices.push(n)
+	  					
+	  					end
+	  				end
+
+	  				if @client.sports[8] << @client.sports[9] << @client.sports[10] == "CBB" 
+
+		  				@cbb_sides.each do |n|
+
+	  					@choices.push(n)
+	  					
+	  					end
+	  				end
+
+	  				if @client.sports[8] << @client.sports[9] << @client.sports[10] == "CFB" 
+
+		  				@cfb_sides.each do |n|
+
+	  					@choices.push(n)
+	  					
+	  					end
+	  				end
+
+	  			
+
+	  			end
+
+		  		@num_choices = @choices.length * 2
+
+		  		if @num_choices == 1
+
+		  			@bet_options = ["straight"]
+		  			@wager_type = @bet_options.sample
+		  			return @num_choices
+
 		  		end
 
-	  			
+		  		if @num_choices == 2
+
+		  			@bet_options = ["straight", "two team teaser", "two team parlay", "two team reverse", "straight"]
+		  			@wager_type = @bet_options.sample
+		  			return @num_choices
+		  		
+		  		end
+
+		  		if @num_choices == 3
+
+		  			@bet_options = ["straight", "two team teaser", "two team parlay", "two team reverse", "straight", "three team teaser", "three team reverse", "three team parlay"]
+		  			@wager_type = @bet_options.sample
+		  			return @num_choices
+
+
+		  		end
+
+		  		if @num_choices == 4
+
+		  			@bet_options = ["straight", "two team teaser", "two team parlay", "two team reverse", "straight", "three team teaser", "three team reverse", "three team parlay", "four team reverse"]
+		  			@wager_type = @bet_options.sample
+		  			return @num_choices
+
+
+		  		end
+
+		  		if @num_choices > 4
+
+		  			@bet_options = ["straight", "two team teaser", "two team parlay", "two team reverse", "straight", "three team teaser", "three team reverse", "three team parlay", "four team reverse", "five team teaser", "five team parlay"]
+		  			@wager_type = @bet_options.sample
+		  			return @num_choices
+
+
+		  		end
+
+	  		end	
+
+	  		
+
+	  		if @client.bet_types == "Exotics"
+
+	  			@choices = [] 
+
+
+	  			if @client.sports[0] << @client.sports[1] << @client.sports[2] == "NFL"
+
+	  				@nfl_sides.each do |n|
+
+	  					@choices.push(n)
+
+	  				end		
+
+	  			end
+
+	  			if @client.sports[0] << @client.sports[1] << @client.sports[2] == "MLB"
+
+	  				@mlb_sides.each do |m|
+
+	  					@choices.push(m)
+	  					
+	  				end
+
+	  			end
+
+	  			if @client.sports[0] << @client.sports[1] << @client.sports[2] == "NBA"
+
+	  				@nba_sides.each do |n|
+
+	  					@choices.push(n)
+	  					
+	  				end
+
+	  			end
+
+	  			if @client.sports[0] << @client.sports[1] << @client.sports[2] == "NHL"
+
+	  				@nhl_sides.each do |n|
+
+	  					@choices.push(n)
+	  					
+	  				end
+
+	  			end
+
+	  			if @client.sports[0] << @client.sports[1] << @client.sports[2] == "CBB"
+
+	  				@cbb_sides.each do |c|
+
+	  					@choices.push(c)
+	  					
+	  				end
+
+	  			end
+
+	  			if @client.sports[0] << @client.sports[1] << @client.sports[2] == "CFB"
+
+	  				@cfb_sides.each do |c|
+
+	  					@choices.push(c)
+	  					
+	  				end
+
+	  			end
+
+	  			if @client.sports.length > 3
+
+		  			if @client.sports[8] << @client.sports[9] << @client.sports[10] == "NFL" 
+
+		  				@nfl_sides.each do |n|
+
+	  					@choices.push(n)
+	  					
+	  					end
+	  				end
+
+
+	  				if @client.sports[8] << @client.sports[9] << @client.sports[10] == "MLB" 
+
+		  				@mlb_sides.each do |m|
+
+	  					@choices.push(m)
+	  					
+	  					end
+	  				end
+
+	  				if @client.sports[8] << @client.sports[9] << @client.sports[10] == "NHL" 
+
+		  				@nhl_sides.each do |n|
+
+	  					@choices.push(n)
+	  					
+	  					end
+	  				end
+
+	  				if @client.sports[8] << @client.sports[9] << @client.sports[10] == "NBA" 
+
+		  				@nba_sides.each do |n|
+
+	  					@choices.push(n)
+	  					
+	  					end
+	  				end
+
+	  				if @client.sports[8] << @client.sports[9] << @client.sports[10] == "CBB" 
+
+		  				@cbb_sides.each do |n|
+
+	  					@choices.push(n)
+	  					
+	  					end
+	  				end
+
+	  				if @client.sports[8] << @client.sports[9] << @client.sports[10] == "CFB" 
+
+		  				@cfb_sides.each do |n|
+
+	  					@choices.push(n)
+	  					
+	  					end
+	  				end  			
+
+	  			end
+
+	  			@num_choices = @choices.length * 2
+
+		  		if @num_choices == 1
+
+		  			@bet_options = ["straight"]
+		  			@wager_type = @bet_options.sample
+		  			return @num_choices
+
+		  		end
+
+		  		if @num_choices == 2
+
+		  			@bet_options = ["two team teaser", "two team parlay", "two team reverse"]
+		  			@wager_type = @bet_options.sample
+		  			return @num_choices
+		  		
+		  		end
+
+		  		if @num_choices == 3
+
+		  			@bet_options = ["two team teaser", "two team parlay", "two team reverse", "three team teaser", "three team reverse", "three team parlay"]
+		  			@wager_type = @bet_options.sample
+		  			return @num_choices
+
+
+		  		end
+
+		  		if @num_choices == 4
+
+		  			@bet_options = ["two team teaser", "two team parlay", "two team reverse", "three team teaser", "three team reverse", "three team parlay", "four team reverse"]
+		  			@wager_type = @bet_options.sample
+		  			return @num_choices
+
+
+		  		end
+
+		  		if @num_choices > 4
+
+		  			@bet_options = ["two team teaser", "two team parlay", "two team reverse", "three team teaser", "three team reverse", "three team parlay", "four team reverse", "five team teaser", "five team parlay"]
+		  			@wager_type = @bet_options.sample
+		  			return @num_choices
+
+
+		  		end
+
+	  		end	
+
+	  		
+
+	  		if @client.bet_types = "Straight"
+
+	  			@wager_type = "straight"
 
 	  		end
-
-	  			
 
 	  	end
 
