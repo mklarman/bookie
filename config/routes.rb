90c36ed8223a887175efb6c498d0fcb1ef5fad92
mklarman <mklarman@hotmail.com>
2018-11-28 10:53:46 -0500

    setting up home page

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ devise_scope :user do
     root :to => "welcome#users_show", as: :authenticated_root
   end
   unauthenticated :user do
-    root :to => 'devise/registrations#new', as: :unauthenticated_root
+    root :to => 'devise/sessions#new', as: :unauthenticated_root
   end
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
