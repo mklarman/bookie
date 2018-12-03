dcc88df865ea989f752a28ff191db46d0753590b
mklarman <mklarman@hotmail.com>
2018-12-03 10:31:30 -0500

    writing helper functions for leagues and pools

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -353,6 +353,136 @@ module ApplicationHelper
 
 		end
 
+		def convert_start_time(date)
+
+			day = date[0] << date[1]
+			
+			month = date[3] << date[4] << date[5]
+
+			if month == "Jan"
+
+				month = "01"
+
+			elsif month == "Feb"
+
+				month = "02"
+
+			elsif month == "Mar"
+
+				month = "03"
+
+			elsif month == "Apr"
+
+				month = "04"
+
+			elsif month == "May"
+
+				month = "05"
+
+			elsif month == "Jun"
+
+				month = "06"
+
+			elsif month == "Jul"
+
+				month = "07"
+
+			elsif month == "Aug"
+
+				month = "08"
+
+			elsif month == "Sep"
+
+				month = "09"
+
+			elsif month == "Oct"
+
+				month = "10"
+
+			elsif month == "Nov"
+
+				month = "11"
+
+			elsif month == "Dec"
+
+				month = "12"
+
+			end
+
+			year = date[7] << date[8] << date[9] << date[10]
+
+			@start_time = day << month << year
+			@start_time = @start_time.to_i
+
+
+
+		end
+
+		def convert_end_time(date)
+
+			day = date[0] << date[1]
+			
+			month = date[3] << date[4] << date[5]
+
+			if month == "Jan"
+
+				month = "01"
+
+			elsif month == "Feb"
+
+				month = "02"
+
+			elsif month == "Mar"
+
+				month = "03"
+
+			elsif month == "Apr"
+
+				month = "04"
+
+			elsif month == "May"
+
+				month = "05"
+
+			elsif month == "Jun"
+
+				month = "06"
+
+			elsif month == "Jul"
+
+				month = "07"
+
+			elsif month == "Aug"
+
+				month = "08"
+
+			elsif month == "Sep"
+
+				month = "09"
+
+			elsif month == "Oct"
+
+				month = "10"
+
+			elsif month == "Nov"
+
+				month = "11"
+
+			elsif month == "Dec"
+
+				month = "12"
+
+			end
+
+			year = date[7] << date[8] << date[9] << date[10]
+
+			@end_time = day << month << year
+			@end_time = @end_time.to_i
+
+
+
+		end
+
 		
 
 
