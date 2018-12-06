d4d4bb35714da9f45fa8d9fe00124f3325ea657f
mklarman <mklarman@hotmail.com>
2018-12-06 10:45:01 -0500

    setting up the flow of leagues

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -26,23 +26,12 @@ class SecretsController < ApplicationController
 
 	def create
 
-		@date = Time.now 
-		@day = @date.strftime("%d")
-		@month = @date.strftime("%b") 
-		@year = @date.strftime("%Y") 
-
-
-		@my_date = @day << " " <<@month << " " <<@year 
 
 		@leagues = League.all 
 		@pools = Pool.all 
 		@page_id
-		my_bool = false
 		secret = Secret.new(secret_params)
 		@pass = secret.pass
-		@current_date
-		@start_date
-		@serial_num_check
 
 		if secret.kind == "league"
 
@@ -51,135 +40,18 @@ class SecretsController < ApplicationController
 				if l.pass == @pass
 
 					@page_id = l.id
-					my_bool = true
-
-					start_day = l.start_date[0] << l.start_date[1]
-					start_month = l.start_date[3] << l.start_date[4] << l.start_date[5]
-					start_year = l.start_date[7] << l.start_date[8] << l.start_date[9] << l.start_date[10]
-
-					current_day = @my_date[0] << @my_date[1]
-					current_month = @my_date[3] << @my_date[4] << @my_date[5]
-					current_year = @my_date[7] << @my_date[8] << @my_date[9] << @my_date[10]
-
-					if start_month == "Jan"
-
-						start_month = "01"
-
-					elsif start_month == "Feb"
-
-						start_month = "02"
-
-					elsif start_month == "Mar"
-
-						start_month = "03"
-
-					elsif start_month == "Apr"
-
-						start_month = "04"
-
-					elsif start_month == "May"
-
-						start_month = "05"
-
-					elsif start_month == "Jun"
-
-						start_month = "06"
-
-					elsif start_month == "Jul"
-
-						start_month = "07"
-
-					elsif start_month == "Aug"
-
-						start_month = "08"
-
-					elsif start_month == "Sep"
-
-						start_month = "09"
-
-					elsif start_month == "Oct"
-
-						start_month = "10"
-
-					elsif start_month == "Nov"
-
-						start_month = "11"
-
-					elsif start_month == "Dec"
-
-						start_month = "12"
-
-					end
-
-					if current_month == "Jan"
-
-						current_month = "01"
-
-					elsif current_month == "Feb"
-
-						current_month = "02"
-
-					elsif current_month == "Mar"
-
-						current_month = "03"
-
-					elsif current_month == "Apr"
-
-						current_month = "04"
-
-					elsif current_month == "May"
-
-						current_month = "05"
-
-					elsif current_month == "Jun"
-
-						current_month = "06"
-
-					elsif current_month == "Jul"
-
-						current_month = "07"
-
-					elsif current_month == "Aug"
-
-						current_month = "08"
-
-					elsif current_month == "Sep"
-
-						current_month = "09"
-
-					elsif current_month == "Oct"
-
-						current_month = "10"
-
-					elsif current_month == "Nov"
-
-						current_month = "11"
-
-					elsif current_month == "Dec"
-
-						current_month = "12"
-
-					end
-
-					@start_date = start_year << start_month << start_year
-					@current_date = current_year << current_month << current_day
-
-					if @start_date.to_i >= @current_date.to_i && my_bool == true
-
-						if secret.save!
-
-								redirect_to league_path(@page_id)
-
-						end
-
-					else
-
-						redirect_to leagues_path
-
-					end
-
-
 				end
+			
+
+			end			
+
+			if secret.save!
+
+				redirect_to league_path(@page_id)
+
+			else
+
+				redirect_to leagues_path
 
 			end
 
@@ -191,36 +63,20 @@ class SecretsController < ApplicationController
 				if p.pass == @pass
 
 					@page_id = p.id
-					my_bool = true
-					self.convert_date(p.date)
-					@serial_num_check = @start_date
-					self.convert_date(secret.date)
-					@serial_num_check = @current_date
-
-
+					
 				end
 
 			end
 
 			if secret.save!
 
-				if my_bool == true && @start_date.to_i > @current_date.to_i
+				redirect_to pool_path(@page_id)
 
-
-					redirect_to pool_path(@page_id)
-
-				else
-
-					redirect_to pools_path
-
-				end
-		
 			else
 
 				redirect_to pools_path
 
 			end
-
 
 		end
 
--- app/helpers/leagues_helper.rb
+++ app/helpers/leagues_helper.rb
@@ -4,39 +4,39 @@ module LeaguesHelper
 
 		if @month_chk == "Jan"
 
-			@month_chk = "1"
+			@month_chk = "01"
 
 		elsif @month_chk == "Feb"
 
-			@month_chk = "2"
+			@month_chk = "02"
 
 		elsif @month_chk == "Mar"
 
-			@month_chk = "3"
+			@month_chk = "03"
 
 		elsif @month_chk == "Apr"
 
-			@month_chk = "4"
+			@month_chk = "04"
 
 		elsif @month_chk == "May"
 
-			@month_chk = "5"
+			@month_chk = "05"
 
 		elsif @month_chk == "Jun"
 
-			@month_chk = "6"
+			@month_chk = "06"
 
 		elsif @month_chk == "Jul"
 
-			@month_chk = "7"
+			@month_chk = "07"
 
 		elsif @month_chk == "Aug"
 
-			@month_chk = "8"
+			@month_chk = "08"
 
 		elsif @month_chk == "Sep"
 
-			@month_chk = "9"
+			@month_chk = "09"
 
 		elsif @month_chk == "Oct"
 
@@ -58,45 +58,103 @@ module LeaguesHelper
 
 	end
 
+	def check_start_date
+
+		if @month_chk == "Jan"
+
+			@month_chk = "01"
+
+		elsif @month_chk == "Feb"
+
+			@month_chk = "02"
+
+		elsif @month_chk == "Mar"
+
+			@month_chk = "03"
+
+		elsif @month_chk == "Apr"
+
+			@month_chk = "04"
+
+		elsif @month_chk == "May"
+
+			@month_chk = "05"
+
+		elsif @month_chk == "Jun"
+
+			@month_chk = "06"
+
+		elsif @month_chk == "Jul"
+
+			@month_chk = "07"
+
+		elsif @month_chk == "Aug"
+
+			@month_chk = "08"
+
+		elsif @month_chk == "Sep"
+
+			@month_chk = "09"
+
+		elsif @month_chk == "Oct"
+
+			@month_chk = "10"
+
+		elsif @month_chk == "Nov"
+
+			@month_chk = "11"
+
+		elsif @month_chk == "Dec"
+
+			@month_chk = "12"
+
+		end
+
+		@start_date_ser = @year_chk + @month_chk + @day_chk  
+		@start_date_ser = @end_date_ser.to_i
+
+
+	end
+
 	def convert_my_date
 
 		@my_month = @my_date[3] + @my_date[4] + @my_date[5] 
 
 		if @my_month == "Jan"
 
-			@my_month = "1"
+			@my_month = "01"
 
 		elsif @my_month == "Feb"
 
-			@my_month = "2"
+			@my_month = "02"
 
 		elsif @my_month == "Mar"
 
-			@my_month = "3"
+			@my_month = "03"
 
 		elsif @my_month == "Apr"
 
-			@my_month = "4"
+			@my_month = "04"
 
 		elsif @my_month == "May"
 
-			@my_month = "5"
+			@my_month = "05"
 
 		elsif @my_month == "Jun"
 
-			@my_month = "6"
+			@my_month = "06"
 
 		elsif @my_month == "Jul"
 
-			@my_month = "7"
+			@my_month = "07"
 
 		elsif @my_month == "Aug"
 
-			@my_month = "8"
+			@my_month = "08"
 
 		elsif @my_month == "Sep"
 
-			@my_month = "9"
+			@my_month = "09"
 
 		elsif @my_month == "Oct"
 
