cf0f634390704e4b26765815f8a75131c9e818e1
mklarman <mklarman@hotmail.com>
2018-12-18 10:50:49 -0500

    building pools

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,10 @@ Rails.application.configure do
   config.logger = Logger.new(STDOUT) 
   config.logger.level = Logger::DEBUG
 
+  logger = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
